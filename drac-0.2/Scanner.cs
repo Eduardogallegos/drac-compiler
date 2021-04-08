5fac480ad438aa734c1af758a479cf50bac6981e
egall <a01745776@itesm.mx>
2021-04-08 12:50:40 -0500

    drac v2

---
+++ drac-0.2/Scanner.cs
@@ -0,0 +1,178 @@
+/*
+  Drac compiler - This class performs the lexical analysis,
+  (a.k.a. scanning).
+  Alejandro Chavez A01374974
+  Pedro Cortes Soberanes A01374919
+  Eduardo Gallegos Solis A01745776
+  ITESM CEM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Drac {
+
+    class Scanner {
+
+        readonly string input;
+
+        static readonly Regex regex = new Regex(
+            @"
+                (?<MultComment>     ([(][*](\n|.)*?[*][)])  )
+              | (?<Comment>         [-][-].*)
+              | (?<Newline>         \n      )
+              | (?<WhiteSpace>      \s      )     # Must go after Newline.
+              | (?<And>             and\b   )
+              | (?<Or>              or\b    )
+              | (?<Dec>             dec\b   )
+              | (?<Inc>             inc\b   )
+              | (?<Break>           break\b )
+              | (?<Do>              do\b    )
+              | (?<Elif>            elif\b  )
+              | (?<Else>            else\b  )
+              | (?<Return>          return\b)
+              | (?<While>           while\b )
+              | (?<Not>             not\b   )
+              | (?<Var>             var\b   )
+              | (?<True>            true\b  )
+              | (?<False>           false\b )
+              | (?<If>              if\b    )
+              | (?<IntLiteral>      -?\d+   )
+              | (?<MoreEqual>       [>][=]  )
+              | (?<LessEqual>       [<][=]  )
+              | (?<Diff>            [<][>]  )
+              | (?<Equals>          [=][=]  )
+              | (?<Assign>          [=]     )
+              | (?<Less>            [<]     )
+              | (?<Greater>         [>]     )
+              | (?<Plus>            [+]     )
+              | (?<Mul>             [*]     )
+              | (?<Neg>             [-]     )
+              | (?<Mod>             [%]     )
+              | (?<Div>             [/]     )
+              | (?<Coma>            [,]     )
+              | (?<Semicolon>       [;]     )
+              | (?<StringLit>       [""]([^\n\\""]|[\\]([nrt\\'""]|u[0-9a-fA-F]{6}))*[""] )
+              | (?<CharLit>         [']([^\n\\']|[\\]([nrt\\'""]|u[0-9a-fA-F]{6}))['] )
+              | (?<ParLeft>         [(]     )
+              | (?<ParRight>        [)]     )
+              | (?<BracketLeft>     [{]     )
+              | (?<BracketRight>    [}]     )
+              | (?<SqrBracketLeft>  [\[]    )
+              | (?<SqrBracketRight> [\]]    )
+              | (?<Identifier>      [a-zA-Z][a-zA-Z0-9_]* )# Must go after all keywords
+              | (?<Other>           .       )     # Must be last: match any other character.
+            ",
+            RegexOptions.IgnorePatternWhitespace
+                | RegexOptions.Compiled
+                | RegexOptions.Multiline
+            );
+
+        static readonly IDictionary<string, TokenCategory> tokenMap =
+            new Dictionary<string, TokenCategory>() {
+                {"And", TokenCategory.AND},
+                {"Dec", TokenCategory.DEC},
+                {"Inc", TokenCategory.INC},
+                {"Break", TokenCategory.BREAK},
+                {"Elif", TokenCategory.ELIF},
+                {"Else", TokenCategory.ELSE},
+                {"Return", TokenCategory.RETURN},
+                {"While", TokenCategory.WHILE},
+                {"Equals", TokenCategory.EQUALS},
+                {"Not", TokenCategory.NOT},
+                {"Var", TokenCategory.VAR},
+                {"Diff", TokenCategory.DIFF},
+                {"Less", TokenCategory.LESS},
+                {"Plus", TokenCategory.PLUS},
+                {"Mul", TokenCategory.MUL},
+                {"Neg", TokenCategory.NEG},
+                {"ParLeft", TokenCategory.PARENTHESIS_OPEN},
+                {"ParRight", TokenCategory.PARENTHESIS_CLOSE},
+                {"Comment", TokenCategory.COMMENT},
+                {"MultComment", TokenCategory.ML_COMMENT},
+                {"Assign", TokenCategory.ASSIGN},
+                {"True", TokenCategory.TRUE},
+                {"False", TokenCategory.FALSE},
+                {"IntLiteral", TokenCategory.INT_LITERAL},
+                {"If", TokenCategory.IF},
+                {"Then", TokenCategory.THEN},
+                {"Identifier", TokenCategory.IDENTIFIER},
+                {"Or", TokenCategory.OR},
+                {"Greater", TokenCategory.GREATER},
+                {"MoreEqual", TokenCategory.MORE_EQUAL},
+                {"LessEqual", TokenCategory.LESS_EQUAL},
+                {"Mod", TokenCategory.MOD},
+                {"Div", TokenCategory.DIV},
+                {"Coma", TokenCategory.COMA},
+                {"Semicolon", TokenCategory.SEMICOLON},
+                {"CharLit", TokenCategory.CHAR_LIT},
+                {"StringLit", TokenCategory.STRING_LIT},
+                {"BracketLeft", TokenCategory.BRACKET_LEFT},
+                {"BracketRight", TokenCategory.BRACKET_RIGHT},
+                {"SqrBracketLeft", TokenCategory.SQR_BRACKET_LEFT},
+                {"SqrBracketRight", TokenCategory.SQR_BRACKET_RIGHT},
+            };
+
+        public Scanner(string input) {
+            this.input = input;
+        }
+
+        public IEnumerable<Token> Scan() {
+
+            var result = new LinkedList<Token>();
+            var row = 1;
+            var columnStart = 0;
+
+            foreach (Match m in regex.Matches(input)) {
+
+                if (m.Groups["Newline"].Success) {
+                    row++;
+                    columnStart = m.Index + m.Length;
+                } else if (m.Groups["WhiteSpace"].Success || m.Groups["Comment"].Success) {
+                    // Skip white space and single line comments.
+                } else if(m.Groups["MultComment"].Success){
+                    foreach (char ch in m.Value){
+                        if (ch == '\n'){
+                            row++;
+                        }
+                    }
+                }else if (m.Groups["Other"].Success) {
+
+                    // Found an illegal character.
+                    result.AddLast(
+                        new Token(m.Value,
+                            TokenCategory.ILLEGAL_CHAR,
+                            row,
+                            m.Index - columnStart + 1));
+
+                } else {
+
+                    // Must be any of the other tokens.
+                    result.AddLast(FindToken(m, row, columnStart));
+                }
+            }
+
+            result.AddLast(
+                new Token(null,
+                    TokenCategory.EOF,
+                    row,
+                    input.Length - columnStart + 1));
+
+            return result;
+        }
+
+        Token FindToken(Match m, int row, int columnStart) {
+            foreach (var name in tokenMap.Keys) {
+                if (m.Groups[name].Success) {
+                    return new Token(m.Value,
+                        tokenMap[name],
+                        row,
+                        m.Index - columnStart + 1);
+                }
+            }
+            throw new InvalidOperationException(
+                "regex and tokenMap are inconsistent: " + m.Value);
+        }
+    }
+}
---
+++ drac-0.2/TokenCategory.cs
@@ -0,0 +1,68 @@
+/*
+  Buttercup compiler - Token categories for the scanner.
+  Copyright (C) 2013 Ariel Ortiz, ITESM CEM
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Drac {
+
+enum TokenCategory {
+        AND,
+        ASSIGN,
+        BREAK,
+        DEC,
+        DO,
+        ELIF,
+        ELSE,
+        EOF,
+        FALSE,
+        IDENTIFIER,
+        IF,
+        INC,
+        INT_LITERAL,
+        LESS,
+        MUL,
+        NEG,
+        PARENTHESIS_OPEN,
+        PARENTHESIS_CLOSE,
+        COMMENT,
+        ML_COMMENT,
+        OR,
+        GREATER,
+        MORE_EQUAL,
+        LESS_EQUAL,
+        MOD,
+        DIV,
+        COMA,
+        SEMICOLON,
+        NOT,
+        CHAR_LIT,
+        STRING_LIT,
+        BRACKET_LEFT,
+        BRACKET_RIGHT,
+        RETURN,
+        SQR_BRACKET_LEFT,
+        SQR_BRACKET_RIGHT,
+        PLUS,
+        THEN,
+        TRUE,
+        ILLEGAL_CHAR,
+        WHILE,
+        VAR,
+        DIFF,
+        EQUALS,
+        NEW_LINE
+    }
+}
