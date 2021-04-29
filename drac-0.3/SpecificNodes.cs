73d0a6979b400d6b86c3fc8eba79408faa3e35bd
egall <a01745776@itesm.mx>
2021-04-29 17:55:05 -0500

    specific nodes init && update make file

---
+++ drac-0.3/SpecificNodes.cs
@@ -0,0 +1,43 @@
+/*
+  Drac compiler - This class performs the lexical analysis,
+  (a.k.a. scanning).
+  Alejandro Chavez A01374974
+  Pedro Cortes Soberanes A01374919
+  Eduardo Gallegos Solis A01745776
+  ITESM CEM
+*/
+
+namespace Drac {
+
+    class Program: Node {}
+
+    class DeclarationList: Node {}
+
+    class Declaration: Node {}
+
+    class StatementList: Node {}
+
+    class Assignment: Node {}
+
+    class Print: Node {}
+
+    class If: Node {}
+
+    class Identifier: Node {}
+
+    class IntLiteral: Node {}
+
+    class True: Node {}
+
+    class False: Node {}
+
+    class Neg: Node {}
+
+    class And: Node {}
+
+    class Less: Node {}
+
+    class Plus: Node {}
+
+    class Mul: Node {}
+}
