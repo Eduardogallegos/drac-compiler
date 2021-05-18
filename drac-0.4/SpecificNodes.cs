57172a2d9af64c40cb74c2918b35cb7b9d5a6399
egall <a01745776@itesm.mx>
2021-05-18 14:23:09 -0500

    init v4

---
+++ drac-0.4/SpecificNodes.cs
@@ -0,0 +1,57 @@
+/*
+  Drac compiler - This class performs the lexical analysis,
+  (a.k.a. scanning).
+  Alejandro Chavez A01374974
+  Pedro Cortes Soberanes A01374919
+  Eduardo Gallegos Solis A01745776
+  ITESM CEM
+*/
+
+namespace Drac
+{
+
+    class Program : Node { }
+    class VarDef : Node { }
+    class Identifier : Node { }
+    class IdList : Node { }
+    class Funcion : Node { }
+    class VarDefList : Node { }
+    class StmtList : Node { }
+    class Assignment : Node { }
+    class FunctionCall : Node { }
+    class Increase : Node { }
+    class Decrease : Node { }
+    class StmtIf : Node { }
+    class ElseIfList : Node { }
+    class ElseIf : Node { }
+    class Else : Node { }
+    class StmtWhile : Node { }
+    class StmtDoWhile : Node { }
+    class StmtBreak : Node { }
+    class StmtReturn : Node { }
+    class StmtEmpty : Node { }
+    class Equals : Node { }
+    class Diff : Node { }
+    class Less : Node { }
+    class LessEqual : Node { }
+    class Greater : Node { }
+    class MoreEqual : Node { }
+    class Neg : Node { }
+    class Plus : Node { }
+    class Mul : Node { }
+    class Div : Node { }
+    class Mod : Node { }
+    class Not : Node { }
+    class Positive : Node { }
+    class Negative : Node { }
+    class exprPrimary : Node { }
+    class True : Node { }
+    class False : Node { }
+    class Int_literal : Node { }
+    class Char_lit : Node { }
+    class String_lit : Node { }
+    class Or : Node { }
+    class And : Node { }
+    class Array : Node { }
+    class ExprList : Node { }
+}
